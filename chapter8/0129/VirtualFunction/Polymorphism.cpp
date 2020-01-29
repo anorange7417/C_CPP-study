14dca54b4020639b003dd489beb5e9711a07082a
yhcho <201929291@snu.ac.kr>
2020-01-29 15:12:53 +0900

    0129 study

---
+++ chapter8/0129/VirtualFunction/Polymorphism.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class First
+{
+public:
+    virtual void SimpleFunc() { cout << "First" << endl; }
+};
+
+class Second : public First
+{
+public:
+    void SimpleFunc() { cout << "Second" << endl; }
+};
+
+int main()
+{
+    First *ptr = new First();
+    ptr->SimpleFunc();
+    delete ptr;
+    ptr = new Second();
+    ptr->SimpleFunc();
+    delete ptr;
+    return 0;
+}
--- chapter8/0129/VirtualFunction/Prob08_1.cpp
+++ chapter8/0129/VirtualFunction/Prob08_1.cpp
@@ -2,6 +2,16 @@
 #include <cstring>
 using namespace std;
 
+namespace RISK_LEVEL
+{
+enum
+{
+    RISK_A = 3,
+    RISK_B = 2,
+    RISK_C = 1
+};
+}
+
 // virtual class
 class Employee
 {
@@ -17,6 +27,7 @@ public:
     {
         cout << "name: " << name << endl;
     }
+    virtual void ShowInfo() const = 0;
 };
 
 // Derived class
@@ -24,14 +35,20 @@ class ForeignSalesWorker : public Employee
 {
 private:
     int salary;
-    int bonusRatio;
+    double bonusRatio;
     int risklevel;
 
 public:
-    ForeignSalesWorker(char *name, int mysalary, int myratio, int myrisklevel) : Employee(name), salary(mysalary), bonusRatio(myratio), risklevel(myrisklevel) {}
+    ForeignSalesWorker(char *name, int mysalary, double myratio, int myrisklevel) : Employee(name), salary(mysalary), bonusRatio(myratio), risklevel(myrisklevel) {}
     void AddSalesResult(int mysale)
     {
         salary += mysale * bonusRatio;
+        salary += salary * bonusRatio * risklevel;
+    }
+    void ShowInfo() const
+    {
+        ShowYourName();
+        cout << "salary: " << salary << endl;
     }
 };
 
@@ -48,4 +65,35 @@ public:
     {
         empList[empNum++] = myemp;
     }
+    void ShowAllSalaryInfo() const
+    {
+        for (int i = 0; i < empNum; i++)
+            empList[i]->ShowInfo();
+    }
+    ~Employeehandler()
+    {
+        for (int i = 0; i < empNum; i++)
+            delete empList[i];
+    }
 };
+
+int main()
+{
+    Employeehandler handler;
+
+    // 해외 영업직 등록
+    ForeignSalesWorker *fseller1 = new ForeignSalesWorker("Hong", 1000, 0.1, RISK_LEVEL::RISK_A);
+    fseller1->AddSalesResult(7000);
+    handler.AddEmployee(fseller1);
+
+    ForeignSalesWorker *fseller2 = new ForeignSalesWorker("Yoon", 1000, 0.1, RISK_LEVEL::RISK_B);
+    fseller2->AddSalesResult(7000);
+    handler.AddEmployee(fseller2);
+
+    ForeignSalesWorker *fseller3 = new ForeignSalesWorker("Lee", 1000, 0.1, RISK_LEVEL::RISK_C);
+    fseller3->AddSalesResult(7000);
+    handler.AddEmployee(fseller3);
+
+    handler.ShowAllSalaryInfo();
+    return 0;
+}
